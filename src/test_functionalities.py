d73caafe9ea154e189df01a0120845f802539790
debajyoti <debajyoti94@gmail.com>
2021-01-12 00:23:47 +0530

    Added doc-strings for unit test cases. Need to add the code now.

---
+++ src/test_functionalities.py
@@ -0,0 +1,35 @@
+''' In this code we will put down the unit test cases '''
+
+import config
+
+class TestFunctionalities:
+
+    def column_null_check(self):
+        '''
+        Function to check if there are any missing
+        values in the columns
+        :return: True if any missing values
+        '''
+        return
+
+    def delimiter_check(self):
+        '''
+        Check if the delimiter same as what is provided in config file
+        :return: True if all is okay
+        '''
+        return
+
+    def test_dataset_shape(self):
+        '''
+        Check if the instances x features match with what is expected
+        :return: True if all is okay
+        '''
+        return
+
+    def test_cleaning_data(self):
+        '''
+        check if the shape of the dataset matches with what is expected
+         after pre-processing the data
+        :return:
+        '''
+        return
