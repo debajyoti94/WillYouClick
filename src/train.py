7a7323bbac1e9e78897cd424cf317022b345c094
debajyoti <debajyoti94@gmail.com>
2021-01-12 01:23:49 +0530

    Added comments for what to do next here.

---
+++ src/train.py
@@ -0,0 +1,41 @@
+'''
+Here we will write code for training the model.
+This code will code contain things associated to adding commandline arguments.
+'''
+
+
+import argparse
+import config
+import create_folds
+
+def run():
+    '''
+    We will train the model here
+    :return:
+    '''
+
+    # train model
+
+    # get metrics
+    return
+
+
+def inference_stage():
+    '''
+    Get the predictions for test set here
+    :return:
+    '''
+    # load model
+
+    # get predictions
+
+    # get performance metrics
+
+    return
+
+if __name__  == "__main__":
+
+    # code for argparse comes here
+
+    # based on commandline arguments
+    # call train / inference stage functions
