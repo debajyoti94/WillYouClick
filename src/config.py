2a632040b1acd4cdb8a353775faa33280ff0aacc
debajyoti <debajyoti94@gmail.com>
2021-01-13 18:11:55 +0530

    Added a few more configuration variables.

--- src/config.py
+++ src/config.py
@@ -4,14 +4,18 @@
 OUTPUT_FEATURE = 'Clicked'
 FEATURES_TO_DROP = ['Names','emails']
 CATEGORICAL_VARIABLES = ['Country']
-TRAIN_FILENAME  = 'train_set.pickle'
-TEST_FILENAME = 'test_set.pickle'
+ORIGINAL_DATASET_FILENAME = '../input/Facebook_Ads_2.csv'
+TRAIN_FILENAME  = '../input/train_set.pickle'
+TEST_FILENAME = '../input/test_set.pickle'
+
+# for plots
+NULL_CHECK_HEATMAP = 'null_check_heatmap.png'
 
 # for cross validation
 NUM_FOLDS = 5
 
 # model name
-MODEL_NAME = 'LR_Ad_click_Baseline_'
+MODEL_NAME = '../models/LR_Ad_click_Baseline_'
 
 FILE_DELIMITER = ','
 ENCODING_TYPE = 'ISO-8859-1'
