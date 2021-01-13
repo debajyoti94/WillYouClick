1fa9b439b8dfff86e18617281cc799217271a7fe
debajyoti <debajyoti94@gmail.com>
2021-01-13 17:06:37 +0530

    Added some more configuration variables.

--- src/config.py
+++ src/config.py
@@ -1,14 +1,17 @@
 ''' This file will contain the variables that will be used for training or other purposes'''
 
-# for feature engineering
+# dataset information
 OUTPUT_FEATURE = 'Clicked'
 FEATURES_TO_DROP = ['Names','emails']
+CATEGORICAL_VARIABLES = ['Country']
+TRAIN_FILENAME  = 'train_set.pickle'
+TEST_FILENAME = 'test_set.pickle'
 
 # for cross validation
 NUM_FOLDS = 5
 
 # model name
-MODEL_NAME = 'LR_Baseline_'
+MODEL_NAME = 'LR_Ad_click_Baseline_'
 
 FILE_DELIMITER = ','
 ENCODING_TYPE = 'ISO-8859-1'
